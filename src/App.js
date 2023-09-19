2f35c9cbee589307814a168030f1dc3b84afd7ac
Tiago Rafael <tiago.rafael019@gmail.com>
2023-09-19 13:33:36 -0300

    done

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Header/> 
         
         <main className="main">
-          {/* <Formulario/> */}
+          {/* <Formulario/>  */}
           <FormHeader/>
         </main>
     </div>
